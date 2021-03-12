45c4db2a04c7fa762bcb7c0fc42cebaa5471409c
Meli Imelda <meliimeldatchouala@gmail.com>
2021-03-12 16:25:37 +0100

    react icons have been installed

--- src/component/Todo.js
+++ src/component/Todo.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
+import {RiCloseCircleLine} from 'react-icons/ri'
 
 function Todo() {
 
@@ -11,7 +12,14 @@ function Todo() {
     return todos.map((todo, index)=>(
         <div className={todo.isComplete ? 'todo-row complete': 'todo-row'} key={index}>
             {/* //in this div we are passing in the todo text */}
-            <div key= {todo.id}
+            <div key= {todo.id} onClick{ () => completeTodo(todo.id)} >
+                {todo.text}
+            </div>
+            {/* this div enables us add react icons to our todo form */}
+            <div className="icons">
+                <RiCloseCircleLine />
+                <TiEdit />
+            </div>
         </div>
 
     ))
